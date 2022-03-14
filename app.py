f3f3571b0c5db198149df9518ba55e9ea825dba0
daffabilnadzary1 <daffabilnadzary1@gmail.com>
2022-03-14 16:43:10 +0700

    working on the architecture (unfinished)

--- app.py
+++ app.py
@@ -58,5 +58,37 @@ def bulk():
 
             conn.send(msg)
 
+s = URLSafeTimedSerializer('Thisisasecret!')
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    if request.method == 'GET':
+        return '<form action = "/" method = "POST"><input name = "email"><input type = "submit"></form>'
+    
+    email = request.form['email']
+    token = s.dumps(email, salt = 'email-confirm')
+
+    msg = Message('Confirmation Email', sender = 'daffabilnadzary1@gmail.com', recipients = [email])
+    link = url_for('confirm_email', token = token, _external = True)
+
+    msg.body = 'Your activation link is {}'.format(link)
+
+    mail.send(msg)
+
+    return '<h1>The email you entered is {}. The token is {}'.format(email, token)
+
+@app.route('/confirm_email/<token>')
+def confirm_email(token):
+    try:
+        email = s.loads(token, salt = 'email-confirm', max_age = 3600)
+
+    except SignatureExpired:
+        return '<h1>The token is expired!</h1>'
+    
+    except BadTimeSignature:
+        return '<h1>The token is incorrect!</h1>'
+
+    return 'The token works!'
+
 if __name__ == "__main__":
     app.run(debug = True)
